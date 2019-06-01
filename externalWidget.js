970676698451ba85a21372d75fd8df34b45956a3
Anton Bobrovskikh <antonb@confirmit.com>
2019-06-01 22:00:45 +0300

    test ext widget

--- externalWidget.js
+++ externalWidget.js
@@ -1,56 +1,54 @@
-define([], function () {
-  var CustomWidget = function () {
-    var self = this;
-    this.callbacks = {
-      render: function(){
-        console.log('render--------ext');
-        return true;
-      },
-      init: function(){
-        console.log('init--------ext---------23333333333  ');
-        return true;
-      },
-      bind_actions: function(){
-        console.log('bind_actions--------ext');
-        return true;
-      },
-      settings: function(){
-        return true;
-      },
-      onSave: function(){
-        alert('click');
-        return true;
-      },
-      destroy: function(){
+define([], function (config) {
+  var CustomWidget = function (config) {
+		var self = this;
 
-      },
-      contacts: {
-        //select contacts in list and clicked on widget name
-        selected: function(){
-          console.log('contacts');
-        }
-      },
-      leads: {
-        //select leads in list and clicked on widget name
-        selected: function(){
-          console.log('leads');
-        }
-      },
-      tasks: {
-        //select taks in list and clicked on widget name
-        selected: function(){
-          console.log('tasks');
-        }
-      }
-    };
-    return this;
-  };
+		this.callbacks = {
+			render: function () {
+				const render_amo_template = (template_name, params) => {
+					return self.render({ ref: '/tmpl/controls/' + template_name + '.twig' }, params);
+				}
 
+				if (self.system().area === 'lcard' && !!AMOCRM.data.current_card && AMOCRM.data.current_card.id) {
+					const data = render_amo_template('button', {
+						text: 'Открыть',
+						class_name: config.widgetCode.replace(/_/g, '-') + config.OPEN_BUTTON_SFX,
+						id: config.widgetCode + config.OPEN_BUTTON_SFX
+					})
+					self.render_template({
+						caption: {
+							class_name: config.widgetCode.replace(/_/g, '-') + config.WIDGET_CAPTION_SFX
+						},
+						body: data,
+						render: '' //data
+					})
+					return true
+				}
+				return false
+			},
+			init: function () {
+				return true;
+			},
+			bind_actions: function () {
+				var data = '<iframe style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;" src="' + config.address + '"></iframe>';
+				const button = $('#' + config.widgetCode + config.OPEN_BUTTON_SFX)
+				button.on('click', function () {
+					var h = document.documentElement.clientHeight - 60 - 48;
+					var w = document.documentElement.clientWidth - 60 - 24;
+					config.create_modal(data, w, h);
+				})
+				return true;
+			},
+			settings: function () {
+				return true;
+			},
+			onSave: function () { return true; },
+			destroy: function () { },
+			contacts: { selected: function () { } },
+			leads: { selected: function () { } },
+			tasks: { selected: function () { } }
+		};
+		return this;
+	};
 
-
-  const module = {
-    widget: CustomWidget
-  }
-
-  return module
+	return CustomWidget;
 });
