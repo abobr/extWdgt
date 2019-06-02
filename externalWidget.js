1c868852a02cf18d20a9bf34ebac1ae80bf8de2c
Anton Bobrovskikh <antonb@confirmit.com>
2019-06-02 12:54:40 +0300

    no-cors

--- externalWidget.js
+++ externalWidget.js
@@ -10,7 +10,7 @@ define(['./test.js'], function (test) {
       // Default options are marked with *
         return fetch(url, {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, cors, *same-origin
+            mode: 'no-cors', // no-cors, cors, *same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             credentials: 'same-origin', // include, *same-origin, omit
             headers: {
